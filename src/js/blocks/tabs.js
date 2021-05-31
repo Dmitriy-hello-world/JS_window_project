ad62094f4f3c61a9d574fddfd2f7ed7f8358c73f
Dmitriy <easportstng@gmail.com>
2021-05-31 22:45:46 +0300

    add tabs js

---
+++ src/js/blocks/tabs.js
@@ -0,0 +1,54 @@
+const tabs = ({tabsSelector,contentsSelector,headerSelector,activeClass, linkSelector}) => {
+
+    const tabs = document.querySelectorAll(tabsSelector),
+          contents = document.querySelectorAll(contentsSelector),
+          header = document.querySelector(headerSelector),
+          links = document.querySelectorAll(linkSelector);
+
+    function hideTabs() {
+        contents.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        if(links.length > 0) {
+            links.forEach(item => {
+                item.classList.remove(activeClass);
+            });
+        } else {
+            tabs.forEach(item => {
+                item.classList.remove(activeClass);
+            });
+        }
+    }
+
+    function showTab(i = 0) {
+        contents[i].style.display = 'block';
+
+        if(links.length > 0) {
+            links[i].classList.add(activeClass);
+        } else {
+            tabs[i].classList.add(activeClass);
+        }
+    }
+
+    hideTabs();
+    showTab();
+
+    header.addEventListener('click', e => {
+        const target = e.target;
+
+        if (target && (target.classList.contains(tabsSelector.replace(/\./, '')) ||
+            target.parentNode.classList.contains(tabsSelector.replace(/\./, '')) )) {
+
+            tabs.forEach((item,index) => {
+                if (item == target || item == target.parentNode) {
+                    hideTabs();
+                    showTab(index);
+                }
+            });
+        }
+    });
+
+};
+
+export default tabs;
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 import './slider';
 import modal from './blocks/modal';
+import tabs from './blocks/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
+    tabs({
+        tabsSelector: '.glazing_block',
+        contentsSelector: '.glazing_content',
+        headerSelector: '.glazing_slider',
+        activeClass: 'active', 
+        linkSelector: '.glazing_block a'
+    });
+    tabs({
+        tabsSelector: '.no_click',
+        contentsSelector: '.decoration_content > div > div',
+        headerSelector: '.decoration_slider',
+        activeClass: 'after_click', 
+        linkSelector: false
+    });
 });
