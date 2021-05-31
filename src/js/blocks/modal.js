3c90b9e0cd07f11d90d9148699b197279b22650e
Dmitriy <easportstng@gmail.com>
2021-05-31 15:38:11 +0300

    add modal js

--- src/js/blocks/modal.js
+++ src/js/blocks/modal.js
@@ -1,5 +1,52 @@
 const modal = () => {
 
+    function bindModal(modalTriggerSelector,modalSelector, closeCroseSelector) {
+        const triggers = document.querySelectorAll(modalTriggerSelector),
+              modal = document.querySelector(modalSelector),
+              crose = document.querySelector(closeCroseSelector);
+
+        triggers.forEach(item => {
+            item.addEventListener('click', e => {
+
+                if (e.target) {
+                    
+                    e.preventDefault();
+
+                    modal.style.display = 'block';
+                    document.body.style.overflow = 'hidden';
+                }
+
+            });
+        });
+
+        crose.addEventListener('click', () => {
+
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+
+        });
+
+        modal.addEventListener('click', e => {
+            if(e.target == modal) {
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+            }
+        });
+    }
+
+    function showModalInTime() {
+        setTimeout(() => {
+
+            document.querySelector('.popup').style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        
+        }, 1000 * 60);
+    }
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalInTime();
+
 };
 
 export default modal;
